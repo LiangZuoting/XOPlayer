967614883f40d0739652b9c5f326fe0a68509cb5
liangzuoting <liangzuoting@gmail.com>
2020-01-14 01:45:15 +0800

    ui update

---
+++ XOPlayer/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerManager : MonoBehaviour, IDropHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData data)
+    {
+        if (data.pointerDrag != null)
+        {
+            Debug.Log("Dropped object was: " + data.pointerDrag);
+        }
+    }
+}
